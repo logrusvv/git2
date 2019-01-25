537c6ecae5ff07c4a81525e09cd6ff770965ce7f
logrusvv <logrusvv@gmail.com>
2019-01-25 11:48:02 +0200

    add SectionType

---
+++ src/lesson3/Main.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+import vovik.java.webapp.model.Contact;
+import vovik.java.webapp.model.ContactType;
+
+/**
+ * Vovik
+ * 1/25/2019
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Contact c = new Contact(ContactType.PHONE,"113131");
+    }
+}
---
+++ src/vovik/java/webapp/model/SectionType.java
@@ -0,0 +1,14 @@
+package vovik.java.webapp.model;
+
+/**
+ * Vovik
+ * 1/25/2019
+ */
+
+public enum SectionType {
+    OBJECTIVE,
+    ACHIEVEMENT,
+    QUALIFICATIONS,
+    EXPERIENCE,
+    EDUCATION
+}
