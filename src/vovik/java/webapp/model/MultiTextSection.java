faf1cff1df93836b1b9a5627f318e1a5beb99a93
logrusvv <logrusvv@gmail.com>
2019-01-25 11:52:23 +0200

    add SectionType

---
+++ src/vovik/java/webapp/model/MultiTextSection.java
@@ -0,0 +1,12 @@
+package vovik.java.webapp.model;
+
+import java.util.List;
+
+/**
+ * Vovik
+ * 1/25/2019
+ */
+
+public class MultiTextSection extends Section {
+    private List<String> values;
+}
