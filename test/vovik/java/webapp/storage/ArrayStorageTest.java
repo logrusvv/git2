2892da88790f8d46641e2c40cd08a673917e7c24
logrusvv <logrusvv@gmail.com>
2019-01-27 14:31:57 +0200

    add JUnit

---
+++ test/vovik/java/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,43 @@
+package vovik.java.webapp.storage;
+
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Vovik
+ * 1/27/2019
+ */
+
+
+public class ArrayStorageTest {
+
+    @org.junit.Test
+    public void clear() {
+    }
+
+    @org.junit.Test
+    public void save() {
+    }
+
+    @org.junit.Test
+    public void update() {
+    }
+
+    @org.junit.Test
+    public void load() {
+    }
+
+    @org.junit.Test
+    public void delete() {
+    }
+
+    @org.junit.Test
+    public void getAllSorted() {
+    }
+
+    @org.junit.Test
+    public void size() {
+    }
+}
